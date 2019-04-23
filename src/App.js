0992c490e15f20759a0870898c1330f109f17d83
ilhan orhan <ilhan.myumyun@sap.com>
2019-04-23 18:49:54 +0300

    chore: remove ui5-toolbar from edit dialog footer (#12)

--- src/App.js
+++ src/App.js
@@ -223,10 +223,10 @@ class App extends Component {
                 <ui5-datepicker format-pattern="dd/MM/yyyy" value={this.state.todoBeingEdittedDate} ref={this.dateEditInput}></ui5-datepicker>
             </div>
           </div>
-            <ui5-toolbar class="dialog-footer" data-ui5-slot="footer">
+            <div className="dialog-footer" >
               <ui5-button type="Transparent" ref={this.cancelBtn}>Cancel</ui5-button>{/*close dialog*/}
               <ui5-button type="Emphasized" ref={this.saveBtn}>Save</ui5-button>{/*save dialog info*/}
-            </ui5-toolbar>
+            </div>
         </ui5-dialog>
       </div>
     );
