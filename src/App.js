c7dbd23f1105f3161d22e11358cfa6cadee86a2d
Martin <martin.r.hristov@gmail.com>
2019-05-22 18:36:22 +0300

    chore: adopt v0.11.0 of UI5 WebComponents (#14)

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ class App extends Component {
   }
 
   handleDone(event) {
-    const selectedItem = event.detail.items[0];
+    const selectedItem = event.detail.selectedItems[0];
     const selectedId = selectedItem.getAttribute("data-key");
 
     const updatedTodos = this.state.todos.map(todo => {
@@ -123,7 +123,7 @@ class App extends Component {
   }
 
   handleUnDone(event) {
-    const selectedItems = event.detail.items;
+    const selectedItems = event.detail.selectedItems;
     const updatedTodos = this.state.todos.map((todo) => {
       const unselectedItem = selectedItems.filter(item => item.getAttribute("data-key") === todo.id.toString());
 
