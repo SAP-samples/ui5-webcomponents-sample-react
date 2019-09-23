fe7a32c2860a8eef84adaf2546cddf674fb3c95a
Merbin J Anselm <merbin.j.anselm@sap.com>
2019-09-23 14:10:41 +0530

    Migrated to new React 16.8 Hooks

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,37 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TodoItem extends Component {
-  constructor(props) {
-    super(props);
-    this.delete = React.createRef();
-    this.edit = React.createRef();
-  }
+function TodoItem ({id, done, hidden, text, deadline, handleDelete, handleEdit}) {
 
-  componentDidMount() {
-    const that = this;
-
-    this.delete.current.addEventListener('press', event => {
-      that.props.handleDelete(that.props.id);
-    });
-
-    this.edit.current.addEventListener('press', event => {
-      that.props.handleEdit(parseInt(that.props.id));
-    });
-  }
-
-  render() {
-    return (
-      <ui5-li-custom key={this.props.id} selected={this.props.done || undefined} data-key={this.props.id} class={this.props.hidden ? "hidden" : ""}>
+  return (
+    <ui5-li-custom key={id} selected={done || undefined} data-key={id} class={hidden ? "hidden" : ""}>
         <div className="li-content">
-          <span className="li-content-text">{this.props.text} - finish before: {this.props.deadline}</span>
+          <span className="li-content-text">{text} - finish before: {deadline}</span>
           <div className="li-content-actions">
-            <ui5-button class="edit-btn" ref={this.edit}>Edit</ui5-button>
-            <ui5-button type="Negative" ref={this.delete}>Delete</ui5-button>
+            <ui5-button class="edit-btn" onClick={handleEdit.bind(this, id)}>Edit</ui5-button>
+            <ui5-button type="Negative" onClick={handleDelete.bind(this, id)}>Delete</ui5-button>
           </div>
         </div>
       </ui5-li-custom>
-    )
-  }
+  );
 }
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,40 +1,30 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem';
+import React, { useRef, useEffect } from "react";
+import TodoItem from "./TodoItem";
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.list = React.createRef();
-    this.deleteBtn = React.createRef();
-  }
+function TodoList({ items, selectionChange, remove, edit }) {
+  const list = useRef();
 
-  componentDidMount() {
-    this.list.current.addEventListener("selectionChange", event => {
-      this.props.selectionChange(event);
-    });
-  }
-  
-  render() {
-    return (
-      <ui5-list id="todo-list" mode="MultiSelect" ref={this.list}>
-        {
-          this.props.items.map((todo) => {
-            return (
-              <TodoItem
-                key={todo.id}
-                id={todo.id}
-                text={todo.text}
-                deadline={todo.deadline}
-                done={todo.done}
-                handleDelete={this.props.remove.bind(this)}
-                handleEdit={this.props.edit.bind(this)}>
-              </TodoItem>
-            )
-          })
-        }
-      </ui5-list>
-    );
-  }
+  useEffect(() => {
+    list.current.addEventListener("selectionChange", selectionChange);
+  });
+
+  return (
+    <ui5-list id="todo-list" mode="MultiSelect" ref={list}>
+      {items.map(todo => {
+        return (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            deadline={todo.deadline}
+            done={todo.done}
+            handleDelete={remove.bind(this)}
+            handleEdit={edit.bind(this)}
+          ></TodoItem>
+        );
+      })}
+    </ui5-list>
+  );
 }
 
 export default TodoList;
