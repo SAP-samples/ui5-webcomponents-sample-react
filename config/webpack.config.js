1db2eeb220e780787c896da1984691f077acc0ce
ilhan <ilhan.myumyun@sap.com>
2021-11-05 10:50:37 +0200

    update to latest

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -446,14 +446,6 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
-            {
-              test: [/cldr\/.*\.json$/, /i18n\/.*\.json$/],
-              loader: 'file-loader',
-              options: {
-                name: 'static/media/[name].[hash:8].[ext]',
-              },
-              type: 'javascript/auto'
-            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
