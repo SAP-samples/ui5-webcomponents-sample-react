0c3ac6881b6cba4a34462185dde2404a1d2a85b5
Marius Obert <1873245+IObert@users.noreply.github.com>
2022-06-03 11:41:25 +0200

    Fix shell logo (#32)

--- src/App.js
+++ src/App.js
@@ -165,8 +165,8 @@ function App () {
   return (
       <div className="app">
         <ui5-shellbar
-          primary-title="UI5 Web Components React Sample Application"
-          logo={logo}>
+          primary-title="UI5 Web Components React Sample Application">
+            <img alt="logo" slot="logo" height="30px" src={logo} />
         </ui5-shellbar>
         <section className="app-content">
           <div className="create-todo-wrapper">
